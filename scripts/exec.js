3a6ebf7739b4911d85b14c18d4a784a5bc905a16
gitdevhp <67614448+gitdevhp@users.noreply.github.com>
2023-05-26 16:58:30 -0500

    diaNum

--- scripts/exec.js
+++ scripts/exec.js
@@ -1,4 +1,13 @@
 console.log("exec.js Loaded");
+var diaNum = 0;
+function PromptNext(){
+if (diaNum=0){
+    document.getElementbyId("Msg").innerHTML= "If you are reading this letter, you have recived 25000 dollars of the amount Dan Cody supposedly gave to you in his will.";
+    document.getElementById("Msg").style.pageBreakAfter = "dollars";
+    dia++
+}
+
+}
 function baseCash(){
 
 }
