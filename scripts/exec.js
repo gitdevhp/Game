382968a4b8af8f4d9be65c52513fa644a34efb58
gitdevhp <67614448+gitdevhp@users.noreply.github.com>
2023-05-26 17:47:28 -0500

    1st Buy

--- scripts/exec.js
+++ scripts/exec.js
@@ -3,6 +3,7 @@ console.log("exec.js Loaded");
 //variables
 var diaNum = 0;
 var cash = 0;
+var genRate;
 
 //functions
 
@@ -10,16 +11,24 @@ function PromptNext(){
 if (diaNum=0){
     document.getElementbyId("Msg").innerHTML= "If you are reading this letter, you have recived 25000 dollars of the amount Dan Cody supposedly gave to you in his will.";
     document.getElementById("Msg").style.pageBreakAfter = "dollars";
-    dia++
+    diaNum++;
 }
 if(diaNum=1){
     document.getElementbyId("Msg").innerHTML=">Narrarator: You have just received a large amount of money. Lets start by starting a small drug store.";
     document.getElementById("Msg").style.pageBreakAfter = "money.";
     baseCash();
+    diaNum++;
 
 }
 }
+
 function baseCash(){
 cash=25000;
 document.getElementById("CashAmt").innerHTML= "Money: " + cash;
+}
+function baseBuy(){
+    cash=cash-25000;
+    genRate=1;
+    document.getElementById("CashAmt").innterHTML="Money: "+cash;
+    console.log("Base Set");
 }
