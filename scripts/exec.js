058d9919d4dfea2a292f9153749b37a50fb4ba77
gitdevhp <67614448+gitdevhp@users.noreply.github.com>
2023-05-26 17:04:39 -0500

    money base

--- scripts/exec.js
+++ scripts/exec.js
@@ -1,13 +1,21 @@
 console.log("exec.js Loaded");
+
+//variables
 var diaNum = 0;
+var cash = 0;
+
+//functions
+
 function PromptNext(){
 if (diaNum=0){
     document.getElementbyId("Msg").innerHTML= "If you are reading this letter, you have recived 25000 dollars of the amount Dan Cody supposedly gave to you in his will.";
     document.getElementById("Msg").style.pageBreakAfter = "dollars";
     dia++
+    baseCash();
 }
 
 }
 function baseCash(){
-
+cash=25000;
+document.getElementById("CashAmt").innerHTML= "Money: " + cash;
 }
