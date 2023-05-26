efce808b256218405b7ee644a79a45c4191e8b28
gitdevhp <67614448+gitdevhp@users.noreply.github.com>
2023-05-26 17:08:54 -0500

    diaframe

--- scripts/exec.js
+++ scripts/exec.js
@@ -11,9 +11,13 @@ if (diaNum=0){
     document.getElementbyId("Msg").innerHTML= "If you are reading this letter, you have recived 25000 dollars of the amount Dan Cody supposedly gave to you in his will.";
     document.getElementById("Msg").style.pageBreakAfter = "dollars";
     dia++
-    baseCash();
 }
+if(diaNum=1){
+    document.getElementbyId("Msg").innerHTML=">Narrarator: You have just received a large amount of money. Lets start by starting a small drug store.";
+    document.getElementById("Msg").style.pageBreakAfter = "money.";
+    baseCash();
 
+}
 }
 function baseCash(){
 cash=25000;
