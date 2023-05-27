7b03d8be368106bf6cf70e194611bc0bf8caa70e
gitdevhp <67614448+gitdevhp@users.noreply.github.com>
2023-05-27 09:47:44 -0500

    u

--- scripts/exec.js
+++ scripts/exec.js
@@ -8,17 +8,16 @@ var genRate;
 //functions
 
 function PromptNext(){
-if (diaNum=0){
+if (diaNum==0){
     document.getElementbyId("Msg").innerHTML= 'If you are reading this letter, you have recived 25000 dollars of the amount Dan Cody supposedly gave to you in his will.';
     document.getElementById("Msg").style.pageBreakAfter = 'dollars';
     diaNum++;
 }
-if(diaNum=1){
+if(diaNum==1){
     document.getElementbyId("Msg").innerHTML='>Narrarator: You have just received a large amount of money. Lets start by starting a small drug store.';
     document.getElementById("Msg").style.pageBreakAfter = 'money.';
     baseCash();
     diaNum++;
-
 }
 }
 
