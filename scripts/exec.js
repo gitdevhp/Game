563ed2e55f88519c4824fe7af9bc8d0f14e5d08b
gitdevhp <67614448+gitdevhp@users.noreply.github.com>
2023-05-27 09:26:50 -0500

    g

--- scripts/exec.js
+++ scripts/exec.js
@@ -9,13 +9,13 @@ var genRate;
 
 function PromptNext(){
 if (diaNum=0){
-    document.getElementbyId("Msg").innerHTML= "If you are reading this letter, you have recived 25000 dollars of the amount Dan Cody supposedly gave to you in his will.";
-    document.getElementById("Msg").style.pageBreakAfter = "dollars";
+    document.getElementbyId("Msg").innerHTML= 'If you are reading this letter, you have recived 25000 dollars of the amount Dan Cody supposedly gave to you in his will.';
+    document.getElementById("Msg").style.pageBreakAfter = 'dollars';
     diaNum++;
 }
 if(diaNum=1){
-    document.getElementbyId("Msg").innerHTML=">Narrarator: You have just received a large amount of money. Lets start by starting a small drug store.";
-    document.getElementById("Msg").style.pageBreakAfter = "money.";
+    document.getElementbyId("Msg").innerHTML='>Narrarator: You have just received a large amount of money. Lets start by starting a small drug store.';
+    document.getElementById("Msg").style.pageBreakAfter = 'money.';
     baseCash();
     diaNum++;
 
@@ -24,11 +24,11 @@ if(diaNum=1){
 
 function baseCash(){
 cash=25000;
-document.getElementById("CashAmt").innerHTML= "Money: " + cash;
+document.getElementById("CashAmt").innerHTML= 'Money: ' + cash;
 }
 function baseBuy(){
     cash=cash-25000;
     genRate=1;
-    document.getElementById("CashAmt").innterHTML="Money: "+cash;
+    document.getElementById("CashAmt").innterHTML='Money: '+cash;
     console.log("Base Set");
 }
