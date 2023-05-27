4aceb3b5bf2ff3d252d8a95d48a16d1f4eaeb006
gitdevhp <67614448+gitdevhp@users.noreply.github.com>
2023-05-27 15:23:23 -0500

    k

--- scripts/exec.js
+++ scripts/exec.js
@@ -7,7 +7,6 @@ var genRate=0;
 var miniB=0;
 //functions
 setInterval(generate,1000);
-document.getElementById("drug").innerHTML.style.visibility = 'hidden';
 function promptNext(){
 if (diaNum==0){
     document.getElementById("msg").innerHTML= '>If you are reading this letter, you have recived 25000 dollars of the amount Dan Cody supposedly gave to you in his will.';
@@ -17,6 +16,7 @@ if (diaNum==0){
 else if(diaNum==1){
     document.getElementById("msg").innerHTML='>Narrarator: You have just received a large amount of money. Lets start by starting a small drug store.';
    // document.getElementById("msg").style.pageBreakAfter = 'money.';
+   document.getElementByID("mini").style.visibility ='visible';
    document.getElementById("mini").innerHTML='Buy Small Drug Store and Small Humble Home';
     baseCash();
     diaNum++;
