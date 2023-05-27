589e1bcd1e8dcba81cd30ac524fec441f8fda2a8
gitdevhp <67614448+gitdevhp@users.noreply.github.com>
2023-05-27 09:53:39 -0500

    test

--- scripts/exec.js
+++ scripts/exec.js
@@ -9,13 +9,13 @@ var genRate;
 
 function PromptNext(){
 if (diaNum==0){
-    document.getElementbyId("Msg").innerHTML= 'If you are reading this letter, you have recived 25000 dollars of the amount Dan Cody supposedly gave to you in his will.';
-    document.getElementById("Msg").style.pageBreakAfter = 'dollars';
+    document.getElementbyId("msg").innerHTML= 'If you are reading this letter, you have recived 25000 dollars of the amount Dan Cody supposedly gave to you in his will.';
+   // document.getElementById("msg").style.pageBreakAfter = 'dollars';
     diaNum++;
 }
 if(diaNum==1){
-    document.getElementbyId("Msg").innerHTML='>Narrarator: You have just received a large amount of money. Lets start by starting a small drug store.';
-    document.getElementById("Msg").style.pageBreakAfter = 'money.';
+    document.getElementbyId("msg").innerHTML='>Narrarator: You have just received a large amount of money. Lets start by starting a small drug store.';
+    //document.getElementById("msg").style.pageBreakAfter = 'money.';
     baseCash();
     diaNum++;
 }
