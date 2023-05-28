a239b5c29817ec9d193a7c613fc20a29ce6c06b2
gitdevhp <67614448+gitdevhp@users.noreply.github.com>
2023-05-27 20:12:16 -0500

    j

--- scripts/exec.js
+++ scripts/exec.js
@@ -31,6 +31,7 @@ else if(diaNum==1) {
 else if(diaNum==2&&cash<10){
     diaNum++
     document.getElementById("msg").innerHTML='>Let us buy some more small stores, its not much but its getting Gatsby closer to Daisy';
+    document.getElementById("NextBut").style.display='none';
 }
 }
 
