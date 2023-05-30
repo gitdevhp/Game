a9df7fb05d7db8c8176c65407f54fcdbb17df4fd
gitdevhp <67614448+gitdevhp@users.noreply.github.com>
2023-05-30 09:25:17 -0500

    m

--- scripts/exec.js
+++ scripts/exec.js
@@ -85,7 +85,7 @@ function buyMini() {
         cash-=miniCost;
         mini++;
         genRate++;
-        miniCost=Math.round(miniCost*1.3);
+        miniCost=Math.round(miniCost*1.5);
         document.getElementById("minip2").innerHTML = "small drug store *"+miniCost+"* ["+mini+"]";
         document.getElementById("NextBut").style.display='block';
         canPass=false;
@@ -94,7 +94,7 @@ function buyMini() {
         cash-=miniCost;
         mini++;
         genRate++;
-        miniCost=Math.round(miniCost*1.4);
+        miniCost=Math.round(miniCost*1.3);
         document.getElementById("minip2").innerHTML = "small drug store |"+miniCost+"| ["+mini+"]";
     }
 }
